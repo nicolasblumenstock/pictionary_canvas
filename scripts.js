f44ac27c8c9aa43edba99483cf071301f98634fe
Nicolas Blumenstock <nicpblum@gmail.com>
2017-05-10 17:46:47 -0400

    adjusted to have a class instead of basic variables

--- scripts.js
+++ scripts.js
@@ -20,7 +20,14 @@ var r = 50;
 // var ySpeed = 3;
 var numberOfClicks = 0;
 
-function ballProject(x,y,xSpeed,ySpeed,xDirection,yDirection){
+function ballProject(x,y,r){
+	this.x = x;
+	this.y = y;
+	this.r = r;
+	this.xSpeed = Math.ceil(Math.random() * 10);
+	this.ySpeed = Math.ceil(Math.random() * 10);
+	this.xDirection = 1;
+	this.yDirection = 1;
 
 }
 
@@ -67,17 +74,20 @@ function drawBall(array){
 
 }
 
-function newBall(array){
-	balls.push({
-		x: 400,
-		y: 250,
-		xDirection: 1,
-		yDirection: 1,
-		xSpeed: Math.ceil(Math.random() * 10),
-		ySpeed: Math.ceil(Math.random() * 10)
-	});
+// function newBall(array){
+// 	array.push({
+// 		x: 400,
+// 		y: 250,
+// 		xDirection: 1,
+// 		yDirection: 1,
+// 		xSpeed: Math.ceil(Math.random() * 10),
+// 		ySpeed: Math.ceil(Math.random() * 10)
+// 	});
+// }
+function newBall(array,x,y,r){
+	array.push(new ballProject(x,y,r));
+	console.log(array)
 }
-
 
 var ball = setInterval(drawBall, 30, balls);
 
@@ -86,11 +96,13 @@ canvas.addEventListener('click', function(event){
 	for (i = 0; i < balls.length; i++){
 		if ((event.offsetX <= balls[i].x + r) && (event.offsetX >= balls[i].x - r) && (event.offsetY <= balls[i].y + r) && (event.offsetY >= balls[i].y - r)){
 			numberOfClicks++;
+			var oldR = r
 			document.getElementById('number').innerHTML = String(numberOfClicks)
 			balls[i].xDirection = -balls[i].xDirection;
 			balls[i].yDirection = -balls[i].yDirection;
 			r -= 5;
-			newBall(balls);
+			newBall(balls,(event.offsetX + oldR),(event.offsetY + oldR),r);
+			newBall(balls,(event.offsetX - oldR),(event.offsetY - oldR),r);
 		}
 	}
 
